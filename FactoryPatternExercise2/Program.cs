ef80f981dfd8a3506a705bf06d6abf4dfd2a2856
Mack McCall <mackmc9924@gmail.com>
2023-08-07 10:54:25 -0600

    completed exercise 2

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What type of database do you want to use? List, SQL, or Mongo?");
+            string userDBType = Console.ReadLine();
+            IDataAccess dataAccess =  DataAccessFactory.GetDataAccessType(userDBType);
+            dataAccess.LoadData();
+            dataAccess.SaveData();
         }
     }
 }
